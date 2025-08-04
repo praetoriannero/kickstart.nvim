aa4e751066a4ba59dd22daa9a8361a86d10a435f
Nicholas Heaven <nick.heaven0930@gmail.com>
2025-08-03 23:03:17 -0500

    mapped alt as window movement modifier

--- init.lua
+++ init.lua
@@ -194,17 +194,17 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<M-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<M-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<M-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<M-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- user-keymap-start
 vim.keymap.set('n', '<leader>T', [[<cmd>vsplit | term<cr>i]], { desc = 'Open new terminal window' })
 vim.keymap.set('n', '<leader>N', [[<cmd>tabnew<cr>]], { desc = 'Open a new tab' })
 vim.keymap.set('n', '<leader>Q', [[<cmd>tabclose<cr>]], { desc = 'Close current tab' })
 vim.keymap.set('n', '<C-b>', [[<cmd>NvimTreeToggle<cr>]], { desc = 'Toggle file tree' })
-vim.keymap.set('n', '<C-q>', '<C-V>')
+-- vim.keymap.set('n', '<C-q>', '<C-V>')
 -- user-keymap-end
 
 -- user-custom-start-commands-begin
