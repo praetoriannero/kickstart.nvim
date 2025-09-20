50f95abe6babcef901a309add1b38ccdc4e80f72
Nicholas Heaven <nick.heaven0930@gmail.com>
2025-09-19 23:33:03 -0500

    made eob = ' '

--- init.lua
+++ init.lua
@@ -214,6 +214,7 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.expandtab = true
   end,
 })
+vim.opt.fillchars = { eob = ' ' }
 -- user-custom-start-commands-begin
 -- vim.api.nvim_create_autocmd({ 'VimEnter' }, {
 --   callback = function()
